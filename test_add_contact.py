3b2ac6e952a3ca620a71090cb3995e4335f242c7
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-08 22:41:11 +0300

    Создал файл c записью теста создания контакта подправил

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-rom selenium import webdriver
-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
-from group import Group
-class TestAddGroup(unittest.TestCase):
+
+
+class TestAddContact(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
@@ -101,23 +102,8 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("home page").click()
         wd.find_element_by_link_text("Logout").click()
 
-    def logout(self, wd):
 
-        wd.find_element_by_link_text("Logout").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
 
     def tearDown(self):
         self.wd.quit()
