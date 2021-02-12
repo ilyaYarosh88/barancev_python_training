e768b0c1e125ad21bf3fecb0f2956f1fc1bd9bfc
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-12 15:08:35 +0300

    Удалили класс, сет ап и тир даун методы. Убрали импорт вебдрайвера и юниттеста

--- test_add_group.py
+++ test_add_group.py
@@ -1,32 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from group import Group
 from application import Application
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
 
-    def test_add_group(self):
-
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(wd, Group(name="testName", header="testHeader", footer="testFooter"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
+@pytest.fixture
+def app(request):
+    fixture = Application
+    request.addfinalazer(fixture.destroy)
+    return fixture
 
 
-    def tearDown(self):
-        self.app.destroy()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(wd, Group(name="testName", header="testHeader", footer="testFooter"))
+    app.logout()
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
+
+
