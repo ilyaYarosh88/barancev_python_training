5b248673eb74e5ce9ee8887d5161956938a7bbd3
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-12 18:01:48 +0300

    Переделал добавление контакта аналогично с созданием группы.

--- test_add_group.py
+++ test_add_group.py
@@ -3,16 +3,18 @@ import pytest
 from group import Group
 from application import Application
 
+
+
 @pytest.fixture
 def app(request):
-    fixture = Application
-    request.addfinalazer(fixture.destroy)
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_group(app):
     app.login(username="admin", password="secret")
-    app.create_group(wd, Group(name="testName", header="testHeader", footer="testFooter"))
+    app.create_group(Group(name="testName", header="testHeader", footer="testFooter"))
     app.logout()
 
 def test_add_empty_group(app):
