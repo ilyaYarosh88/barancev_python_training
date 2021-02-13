4e1a26cc9e50736f94ec2eeedd86bc8396f177e0
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-13 20:30:11 +0300

    conftest
    Создали файл конфтест
    Удалили фикстуру из файлов с тестами.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,9 @@
 # -*- coding: utf-8 -*-
-import pytest
 from model.contact import Contact
-from fixture.application import Application
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-    
+
+
 def test_create_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Ivan", middlename="Sergeevich", lastname="Petrov", nickname="Butthead",
