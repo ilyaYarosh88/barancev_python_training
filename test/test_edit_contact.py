37db4f85011041aeef7b31de717271bd4d242389
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-13 23:13:20 +0300

    Update test_edit_contact.py

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -10,6 +10,6 @@ def test_edit_first_contact(app):
                             title="Editedtest", company="EditedGazprom", address="EditedMoscow", home="1111",
                             mobile="1111", work="1111", fax="1111",
                             email="Editedispetrov@mail.ru", email2="Editedispetrov2@mail.ru", email3="Editedispetrov3@mail.ru",
-                            homepage="Editedwww.petrov.su", bday="3", bmonth="April", byear="1974", aday="9", amonth="May",
+                            homepage="Editedwww.petrov.su", bday="9", bmonth="May", byear="1974", aday="9", amonth="May",
                             ayear="2000", address2="EditedMoscow", phone2="1111", notes="EditedTest"))
     app.session.logout()
