b3cfbd6597c761c5d0d8328fe545095320313464
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-15 14:17:56 +0300

    Выделил методы заполнения полей для методов создания и редактирования групп и контактов.

--- fixture/group.py
+++ fixture/group.py
@@ -16,15 +16,7 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_group_firms(group)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -47,6 +39,17 @@ class GroupHelper:
         # init group edit
         wd.find_element_by_name("edit").click()
         # edit group firms
+        self.fill_group_firms(group)
+        # submit group update
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def fill_group_firms(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -56,10 +59,3 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group update
-        wd.find_element_by_name("update").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        wd = self.app.wd
-        wd.find_element_by_link_text("group page").click()
