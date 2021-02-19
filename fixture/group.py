38f023ae5757fd37e59dfc4a5ee5f0e15caafb14
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-19 21:07:50 +0300

    modify group

--- fixture/group.py
+++ fixture/group.py
@@ -31,7 +31,7 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
-    def edit_first_group(self, group):
+    def modify_first_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
         # select first group
---
+++ test/test_modify_group.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+
+def test_modify_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="EditedGroup", header="EditedHeader", footer="EditedFooter"))
+    app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="EditedGroup"))
+    app.session.logout()
