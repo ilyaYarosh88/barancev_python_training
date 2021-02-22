7f405d00f603584007d6fa75106fa2f117e0a140
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-22 15:59:36 +0300

    Починил метод def test_delete_first_group(app): Пропустил скобочки

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -3,6 +3,6 @@ from model.group import Group
 
 
 def test_delete_first_group(app):
-    if app.group.count == 0:
-        app.group.create(Group(name="test"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="Name"))
     app.group.delete_first_group()
