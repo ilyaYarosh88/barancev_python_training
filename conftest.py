d3ea30144b1f91d43b52d3ed49909ca3055f725d
ilyaYarosh88 <iyarosh88@mail.ru>
2021-02-22 11:33:58 +0300

    разделили фикстуру

--- conftest.py
+++ conftest.py
@@ -14,8 +14,10 @@ def app(request):
         if not fixture.is_valid():
             fixture = Application()
             fixture.session.login(username="admin", password="secret")
+    return fixture
 
-
+@pytest.fixture(scope="session", autouse= True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.destroy()
