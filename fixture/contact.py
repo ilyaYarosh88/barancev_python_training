5303411068385c1d6798e85121b240df7bf42cd7
ilyaYarosh88 <iyarosh88@mail.ru>
2021-03-23 19:19:45 +0300

    Добавлены asserts во все тесты

--- fixture/contact.py
+++ fixture/contact.py
@@ -134,10 +134,10 @@ class ContactHelper:
         wd = self.app.wd
         self.open_home_page()
         contacts = []
-        for element in wd.find_elements_by_css_selector("tr.entry"):
-            cells = element.find_elements_by_tagname("td")
+        for row in wd.find_elements_by_name("entry"):
+            cells = row.find_elements_by_tag_name("td")
             firstname = cells[2].text
             lastname = cells[1].text
-            contact_id = element.find_element_by_name("selected[]").get_attribute("value")
-            contacts.append(Contact(Contact_id=contact_id, lastname=lastname, firstname=firstname, id=id))
+            contact_id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+            contacts.append(Contact(id=contact_id, lastname=lastname, firstname=firstname))
         return contacts
