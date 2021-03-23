7ca4e545b0a78dae7af8317418ca23215e14ac96
ilyaYarosh88 <iyarosh88@mail.ru>
2021-03-23 17:44:54 +0300

    Исправили репр

--- model/contact.py
+++ model/contact.py
@@ -5,7 +5,7 @@ class Contact:
     def __init__(self, firstname, middlename, lastname, nickname, title,
                  company, address, home, mobile, work, fax, email, email2,
                  email3, homepage, bday, bmonth, byear, aday, amonth, ayear,
-                 address2, phone2, notes):
+                 address2, phone2, notes, id):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -30,9 +30,10 @@ class Contact:
         self.address2 = address2
         self.phone2 = phone2
         self.notes = notes
+        self.id = id
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
