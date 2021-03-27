439ead4521f95ed41626ea1a9f4e362dedc00a1c
ilyaYarosh88 <iyarosh88@mail.ru>
2021-03-27 17:45:11 +0300

    Изменили телефоны в моделе согласно видео, что бы не путаться.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -7,11 +7,11 @@ from model.contact import Contact
 def test_add_contact(app):
     old_contact_list = app.contact.get_contact_list()
     contact = Contact(firstname="Ivan", middlename="Sergeevich", lastname="Petrov", nickname="Butthead",
-                            title="test", company="Gazprom", address="Moscow", home="+74950000000",
-                            mobile="+79190000000", work="+74951000000", fax="+74952000000",
-                            email="ispetrov@mail.ru", email2="ispetrov2@mail.ru", email3="ispetrov3@mail.ru",
-                            homepage="www.petrov.su", bday="2", bmonth="April", byear="1973", aday="6", amonth="May",
-                            ayear="1999", address2="Moscow", phone2="1", notes="Test")
+                      title="test", company="Gazprom", address="Moscow", homephone="+74950000000",
+                      mobilephone="+79190000000", workphone="+74951000000", fax="+74952000000",
+                      email="ispetrov@mail.ru", email2="ispetrov2@mail.ru", email3="ispetrov3@mail.ru",
+                      homepage="www.petrov.su", bday="2", bmonth="April", byear="1973", aday="6", amonth="May",
+                      ayear="1999", address2="Moscow", secondaryphone="1", notes="Test")
     app.contact.add_contact(contact)
 
     assert len(old_contact_list) + 1 == app.contact.count()
