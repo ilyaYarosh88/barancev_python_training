1a12b891f11f07f68a00d3182a418516e17ef6d8
ilyaYarosh88 <iyarosh88@mail.ru>
2021-04-04 19:55:40 +0300

    Параметризировал тест создания контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,7 +14,15 @@ def random_string(prefix, maxlen):
 
 
 testdata = [Contact(firstname="", lastname="")] + [
-                Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10))
+                Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), nickname=random_string("nickname",10),
+                        title=random_string("title", 10), company=random_string("company", 10), address=random_string("address", 10),
+                        home=random_string("9190000000", 10), mobile=random_string("9190000000", 10), work=random_string("9190000000", 10),
+                        fax=random_string("9190000000", 10), email=random_string("email@mail.im", 10), email2=random_string("email2@mail.im", 10),
+                        email3=random_string("email@mail3.im", 10), homepage=random_string("www.homepage.ru", 10), address2=random_string("address2", 10),
+                        phone2=random_string("phone2", 10), notes=random_string("notes", 10)
+
+
+)
                 for i in range(5)]
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
