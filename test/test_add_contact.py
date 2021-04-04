f9bc422e1f75c3a395f36b3893b21c39725f9387
ilyaYarosh88 <iyarosh88@mail.ru>
2021-04-04 20:10:31 +0300

    Параметризировал тест создания контакта

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,18 +11,18 @@ def random_string(prefix, maxlen):
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-
-
-testdata = [Contact(firstname="", lastname="")] + [
+testdata = [Contact(firstname="", lastname="", nickname="",
+                        title="", company="", address="",
+                        home="", mobile="", work="",
+                        fax="", email="", email2="",
+                        email3="", homepage="", address2="",
+                        phone2="", notes="")] + [
                 Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10), nickname=random_string("nickname",10),
                         title=random_string("title", 10), company=random_string("company", 10), address=random_string("address", 10),
                         home=random_string("9190000000", 10), mobile=random_string("9190000000", 10), work=random_string("9190000000", 10),
                         fax=random_string("9190000000", 10), email=random_string("email@mail.im", 10), email2=random_string("email2@mail.im", 10),
                         email3=random_string("email@mail3.im", 10), homepage=random_string("www.homepage.ru", 10), address2=random_string("address2", 10),
-                        phone2=random_string("phone2", 10), notes=random_string("notes", 10)
-
-
-)
+                        phone2=random_string("phone2", 10), notes=random_string("notes", 10))
                 for i in range(5)]
 
 @pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
