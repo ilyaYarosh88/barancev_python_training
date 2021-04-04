f514741f862016a0c1d0e86d43e36e68cb7bfd13
ilyaYarosh88 <iyarosh88@mail.ru>
2021-04-04 19:15:06 +0300

    Параметризация закоменчена. Есть параметризация только ластнейм и фёрстнейм

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,21 +4,36 @@ import pytest
 import random
 import string
 
+
+
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
+
+
+
 testdata = [Contact(firstname="", lastname="")] + [
-               Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10))
-               for i in range(5)
-           ]
+                Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10))
+                for i in range(5)]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
+    old_contact_list = app.contact.get_contact_list()
+    app.contact.add_contact(contact)
+    assert len(old_contact_list) + 1 == app.contact.count()
+    new_contact = app.contact.get_contact_list()
+    old_contact_list.append(contact)
+    assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
+
+
 # testdata = [
 #     Contact(firstname=firstname, middlename=middlename, lastname=lastname, nickname=nickname, title=title,
-#                      company=company, address=address, home=home, mobile=mobile, email=email, email2=email2,
-#                      email3=email3, bday=bday, bmonth=bmonth,
-#                      byear=byear, address2=address2, phone2=phone2, notes=notes)
-#     for lastname in ["" ,random_string("lastname", 15)]
+#                       company=company, address=address, home=home, mobile=mobile, work=work, fax=fax, email=email, email2=email2,
+#                       email3=email3, homepage=homepage, bday=bday, bmonth=bmonth,
+#                       byear=byear, ayear=ayear, amonth=amonth, aday=aday, address2=address2, phone2=phone2, notes=notes)
 #     for firstname in ["", random_string("firstname", 10)]
+#     for lastname in ["" ,random_string("lastname", 15)]
 #     for middlename in ["", random_string("middlename", 15)]
 #     for nickname in ["", random_string("nickname", 10)]
 #     for title  in ["", random_string("title", 20)]
@@ -32,29 +47,12 @@ testdata = [Contact(firstname="", lastname="")] + [
 #     for email2 in ["", random_string("email2", 20)]
 #     for email3 in ["", random_string("email3", 20)]
 #     for homepage in ["", random_string("homepage", 20)]
-#     for byear in ["", random_string("byear", 20)]
-#     for bmonth in ["-", random_string("bmonth", 20)]
+#     for byear in ["", str(randint(1, 31))]
+#     for bmonth in ["-", str(randint(1, 12))]
 #     for bday in ["", random_string("bday", 20)]
 #     for ayear in ["", random_string("ayear", 4)]
-#     for amonth in ["", random_string("amonth", 2)]
-#     for aday in ["", str(randint(1, 31)]
+#     for amonth in ["", str(randint(1, 12))]
+#     for aday in ["", str(randint(1, 31))]
 #     for address2 in ["", random_string("title", 20)]
 #     for phone2 in ["", random_string("title", 20)]
-#     for notes in ["", random_string("title", 20)]
-# ]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
-    old_contact_list = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Sergeevich", lastname="Petrov", nickname="Butthead", title="test",
-                      company="Gazprom", address="Moscow", home="+74950000000", mobile="+79190000000",
-                      work="+74951000000", fax="+74952000000", email="ispetrov@mail.ru", email2="ispetrov2@mail.ru",
-                      email3="ispetrov3@mail.ru", homepage="www.petrov.su", bday="2", bmonth="April", byear="1973",
-                      aday="6", amonth="May", ayear="1999", address2="Moscow", phone2="+5", notes="Test")
-    app.contact.add_contact(contact)
-
-    assert len(old_contact_list) + 1 == app.contact.count()
-    new_contact = app.contact.get_contact_list()
-    old_contact_list.append(contact)
-    assert sorted(old_contact_list, key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
+#     for notes in ["", random_string("title", 20)]]
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,6 +12,7 @@ testdata = [ Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
     for i in range(5)
 ]
+
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
     old_groups = app.group.get_group_list()
